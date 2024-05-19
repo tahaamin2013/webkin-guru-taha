6d8d20ee4c6dfa57fd209ce8aa9130a075b19274
tahaamin2013 <tahaamindob2013@gmail.com>
2024-05-19 23:24:51 +0500

    fdsffsd

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -33,9 +33,27 @@ const AboutUs: React.FC = () => {
           <span className="text-primary">engineers and solution providers</span>
         </div>
       </div>
-      fsdfsd
+      <div className="px-5 md:px-[120px] mt-3">
+        <button className="rounded-sm  py-1 text-black text-md px-2 border-2 border-primary">
+          About Us
+        </button>
+        <h1 className="text-4xl font-bold text-[#1a0f2e]">
+          Devsinc helps start-ups, SMEs <br /> and enterprises grow their <br />{" "}
+          business
+        </h1>
+        <div className="flex flex-col md:flex-row justify-between">
+          <p className="text-md mt-2 text-zinc-700 font-sans">
+            We’ve been helping customers since 2009 and take pride in delivering
+            high-quality custom <br /> services designed to help you build,
+            grow, and revolutionize your business.
+          </p>
+          <button className="group text-white bg-primary hover:border-2 hover:bg-transparent transition-all duration-300 hover:border-primary justify-between  hover:text-black rounded-full px-5 py-4 flex items-center gap-9 font-bold">
+            Learn More
+            <div className="mt-0.5 p-1 bg-white rounded-full group-hover:bg-black transition-all duration-300"></div>
+          </button>
+        </div>
+      </div>
     </section>
   );
 };
-
 export default AboutUs;
