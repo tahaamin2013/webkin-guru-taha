700d257d55f289a3bf2ce3673b2c92f60afbf12b
tahaamin2013 <tahaamindob2013@gmail.com>
2024-05-20 15:55:07 +0500

    hide a icon

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -96,12 +96,6 @@ const AboutUs: React.FC = () => {
             Our Values
           </button>
           <div className="grid grid-cols-1 md:grid-cols-2 md:border-0 border shadow-xl rounded-lg md:shadow-none bg-white p-4 mt-3 mb-3 mx-3 md:p-0 md:bg-transparent   justify-between items-center">
-            <div className="flex justify-center items-center md:hidden">
-              <div className="values-image object-contain">
-                <div className="bg-[#00d5b9] opacity-[.24] rounded-full w-[300px] h-[300px] absolute  blur-2xl"></div>
-                <img src="./idea.webp" loading="lazy" width={280} alt="" />
-              </div>
-            </div>
             <div className="mt-5">
               <h1 className="font-bold text-5xl mb-4">Innovation</h1>
               <p className="text-md mt-2 text-zinc-600 font-sans">
